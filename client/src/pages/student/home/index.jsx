844ae92c0fc5423b913b60c2ccfbf1677804c303
Eya Abidi <abidi.eya001@gmail.com>
2024-11-03 01:27:48 +0100

    init

---
+++ client/src/pages/student/home/index.jsx
@@ -0,0 +1,79 @@
+import { courseCategories } from "@/config";
+import banner from "../../../../public/banner-img.png";
+import { Button } from "@/components/ui/button";
+import { useContext, useEffect } from "react";
+import { StudentContext } from "@/context/student-context";
+import {
+  checkCoursePurchaseInfoService,
+  fetchStudentViewCourseListService,
+} from "@/services";
+import { AuthContext } from "@/context/auth-context";
+import { useNavigate } from "react-router-dom";
+
+function StudentHomePage() {
+  const { studentViewCoursesList, setStudentViewCoursesList } =
+    useContext(StudentContext);
+  const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  function handleNavigateToCoursesPage(getCurrentId) {
+    console.log(getCurrentId);
+    sessionStorage.removeItem("filters");
+    const currentFilter = {
+      category: [getCurrentId],
+    };
+
+    sessionStorage.setItem("filters", JSON.stringify(currentFilter));
+
+    navigate("/courses");
+  }
+
+  async function fetchAllStudentViewCourses() {
+    const response = await fetchStudentViewCourseListService();
+    if (response?.success) setStudentViewCoursesList(response?.data);
+  }
+
+  async function handleCourseNavigate(getCurrentCourseId) {
+    const response = await checkCoursePurchaseInfoService(
+      getCurrentCourseId,
+      auth?.user?._id
+    );
+
+    if (response?.success) {
+      if (response?.data) {
+        navigate(`/course-progress/${getCurrentCourseId}`);
+      } else {
+        navigate(`/course/details/${getCurrentCourseId}`);
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchAllStudentViewCourses();
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-white">
+      <section className="flex flex-col lg:flex-row items-center justify-between py-8 px-4 lg:px-8">
+        <div className="lg:w-1/2 lg:pr-12">
+          <h1 className="text-4xl font-bold mb-4">Education without Limits</h1>
+          <p className="text-xl">
+          Knowledge for Everyone, Everywhere. <br/> Get Started with US
+          </p>
+        </div>
+        <div className="lg:w-full mb-8 lg:mb-0">
+          <img
+            src={banner}
+            width={600}
+            height={400}
+            className="w-full h-auto rounded-lg shadow-lg"
+          />
+        </div>
+      </section>
+
+
+    </div>
+  );
+}
+
+export default StudentHomePage;
